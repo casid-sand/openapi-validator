846e154410793b10b8a5d02fdd266d511c93ef51
mikymikymike <mickael.bocquet@gmail.com>
2021-02-24 14:38:28 +0100

    vérification de la présence d'un path health

--- test/mock-validation/run-swagger-validation.js
+++ test/mock-validation/run-swagger-validation.js
@@ -14,7 +14,7 @@ swagger_file_name = 'CMan.yaml';
 swagger_file_name = 'Swagger-ds.yml';
 swagger_file_name = 'swagger-DNA-2-12-2020-IntradefToInternet.yml';
 
-swagger_file_name = '2021-02-12-swagger-ATRIUM.yaml';
+swagger_file_name = 'swagger2-SYRIARM-V1-1.0.json';
 //swagger_file_name = 'openapi-demo.yaml';
 
 
