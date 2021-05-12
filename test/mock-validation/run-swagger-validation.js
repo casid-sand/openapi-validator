3c6fdabc3b76ef91a434cef008086ca9dedaf38a
mikymikymike <mickael.bocquet@gmail.com>
2021-05-12 17:51:23 +0200

    modifications mineures

--- test/mock-validation/run-swagger-validation.js
+++ test/mock-validation/run-swagger-validation.js
@@ -1,5 +1,6 @@
 const commandLineValidator = require('../../src/cli-validator/runValidator');
 const dateTime = require('node-datetime');
+const fileExtension = /^(.*)(\.([^.]+))$/;
 
 const program = {};
 let exitcode;
@@ -7,30 +8,19 @@ let exitcode;
 let dir_path = './test/mock-validation/input/';
 let swagger_file_name;
 
-swagger_file_name = 'openapi3-test.yaml';
-swagger_file_name = 'swagger-test.yml';
-
-swagger_file_name = 'CMan.yaml';
-swagger_file_name = 'Swagger-ds.yml';
-swagger_file_name = 'swagger-DNA-2-12-2020-IntradefToInternet.yml';
-
-swagger_file_name = 'RPPS_api_v1.0.yaml';
-//swagger_file_name = 'openapi-demo.yaml';
+swagger_file_name = 'swagger_SourceWebRH-v0.2-12_05_2021.yaml';
 
 
-//swagger_file_name = "missing-object.yml"
 //dir_path = "./test/cli-validator/mockFiles/";
 
 
 let swagger_name = '';
-let swagger_ext = '';
-swagger_file_name.split('.').map(filename_part => {
-    if (swagger_name == '') {
-        swagger_name = filename_part;
-    } else {
-        swagger_ext = filename_part;
-    }
-});
+if (fileExtension.test(swagger_file_name)) {
+    const fileNameParts = fileExtension.exec(swagger_file_name);
+    swagger_name = fileNameParts[1];
+} else {
+    swagger_name = swagger_file_name;
+}
 
 var dt = dateTime.create();
 var formattedDate = dt.format('Y-m-d');
